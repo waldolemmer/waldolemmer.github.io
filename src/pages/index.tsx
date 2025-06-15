d57e8356a99ec14f2b7fd454dd1d40f5bfab82b4
Waldo Lemmer <141508260+waldolemmer@users.noreply.github.com>
2025-06-15 20:26:56 +0200

    finalize showcase (#15)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,85 @@
+//import type {ReactNode} from 'react';
+//import clsx from 'clsx';
+//import Link from '@docusaurus/Link';
+//import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+//import Layout from '@theme/Layout';
+//import HomepageProjects from '@site/src/components/HomepageProjects';
+//import Heading from '@theme/Heading';
+//
+//import styles from './index.module.css';
+//
+//function HomepageHeader() {
+//  const {siteConfig} = useDocusaurusContext();
+//  return (
+//    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+//      <div className="container">
+//        <Heading as="h1" className="hero__title">
+//          {siteConfig.title}
+//        </Heading>
+//        <p className="hero__subtitle">{siteConfig.tagline}</p>
+//        <div className={styles.buttons}>
+//	{/*<Link
+//            className="button button--secondary button--lg"
+//            to="/docs/intro">
+//            Docusaurus Tutorial - 5min ⏱️
+//          </Link>*/}
+//        </div>
+//      </div>
+//    </header>
+//  );
+//}
+//
+//export default function Home(): ReactNode {
+//  const {siteConfig} = useDocusaurusContext();
+//  return (
+//    <Layout
+//      title={`${siteConfig.title}`}
+//      description="Waldo Lemmer's home page">
+//      <HomepageHeader />
+//      <main>
+//        <HomepageProjects />
+//      </main>
+//    </Layout>
+//  );
+//}
+///**
+// * Copyright (c) Facebook, Inc. and its affiliates.
+// *
+// * This source code is licensed under the MIT license found in the
+// * LICENSE file in the root directory of this source tree.
+// */
+
 import type {ReactNode} from 'react';
-import clsx from 'clsx';
+import Translate, {translate} from '@docusaurus/Translate';
+
 import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageProjects from '@site/src/components/HomepageProjects';
 import Heading from '@theme/Heading';
 
-import styles from './index.module.css';
+import ShowcaseCards from './showcase/_components/ShowcaseCards';
+import ShowcaseFilters from './showcase/_components/ShowcaseFilters';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+const TITLE = translate({message: 'Waldo Lemmer'});
+const DESCRIPTION = translate({
+  message: 'My projects and guides',
+});
+
+function ShowcaseHeader() {
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-	{/*<Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>*/}
-        </div>
-      </div>
-    </header>
+    <section className="margin-top--lg margin-bottom--lg text--center">
+      <Heading as="h1">{TITLE}</Heading>
+      <p>{DESCRIPTION}</p>
+    </section>
   );
 }
 
-export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
+export default function Showcase(): ReactNode {
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Waldo Lemmer's home page">
-      <HomepageHeader />
-      <main>
-        <HomepageProjects />
+    <Layout title={TITLE} description={DESCRIPTION}>
+      <main className="margin-vert--lg">
+        <ShowcaseHeader />
+        <ShowcaseFilters />
+        <ShowcaseCards />
       </main>
     </Layout>
   );
