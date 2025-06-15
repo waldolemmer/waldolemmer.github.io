e46820774b81eb62dbb47c3abf655011e1ea6047
waldolemmer <141508260+waldolemmer@users.noreply.github.com>
2025-06-15 19:53:57 +0200

    add showcase to homepage

---
+++ src/pages/showcase/_components/ClearAllButton/index.tsx
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React, {type ReactNode} from 'react';
+import {useClearQueryString} from '@docusaurus/theme-common';
+import sharedStyles from '../styles.module.css';
+
+export default function ClearAllButton(): ReactNode {
+  const clearQueryString = useClearQueryString();
+  // TODO translate
+  return (
+    <button
+      className="button button--outline button--primary"
+      type="button"
+      onClick={() => clearQueryString()}>
+      Clear All
+    </button>
+  );
+}
