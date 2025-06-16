915be9ec1e062302732f5b2ddff21ffe1fe50755
waldolemmer <141508260+waldolemmer@users.noreply.github.com>
2025-06-16 13:32:09 +0200

    improve homepage description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,3 @@
-//import type {ReactNode} from 'react';
-//import clsx from 'clsx';
-//import Link from '@docusaurus/Link';
-//import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-//import Layout from '@theme/Layout';
-//import HomepageProjects from '@site/src/components/HomepageProjects';
-//import Heading from '@theme/Heading';
-//
-//import styles from './index.module.css';
-//
-//function HomepageHeader() {
-//  const {siteConfig} = useDocusaurusContext();
-//  return (
-//    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-//      <div className="container">
-//        <Heading as="h1" className="hero__title">
-//          {siteConfig.title}
-//        </Heading>
-//        <p className="hero__subtitle">{siteConfig.tagline}</p>
-//        <div className={styles.buttons}>
-//	{/*<Link
-//            className="button button--secondary button--lg"
-//            to="/docs/intro">
-//            Docusaurus Tutorial - 5min ⏱️
-//          </Link>*/}
-//        </div>
-//      </div>
-//    </header>
-//  );
-//}
-//
-//export default function Home(): ReactNode {
-//  const {siteConfig} = useDocusaurusContext();
-//  return (
-//    <Layout
-//      title={`${siteConfig.title}`}
-//      description="Waldo Lemmer's home page">
-//      <HomepageHeader />
-//      <main>
-//        <HomepageProjects />
-//      </main>
-//    </Layout>
-//  );
-//}
-///**
-// * Copyright (c) Facebook, Inc. and its affiliates.
-// *
-// * This source code is licensed under the MIT license found in the
-// * LICENSE file in the root directory of this source tree.
-// */
-
 import type {ReactNode} from 'react';
 import Translate, {translate} from '@docusaurus/Translate';
 
@@ -61,7 +10,8 @@ import ShowcaseFilters from './showcase/_components/ShowcaseFilters';
 
 const TITLE = translate({message: 'Waldo Lemmer'});
 const DESCRIPTION = translate({
-  message: 'My projects and guides',
+  message: 'My projects and guides, covering software development, Linux and \
+            system administration, electronics and more.',
 });
 
 function ShowcaseHeader() {
