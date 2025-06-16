52639a1468e1a452665393dd8c6efdf3ba6ce0b2
waldolemmer <141508260+waldolemmer@users.noreply.github.com>
2025-06-16 13:50:44 +0200

    add image meta tags to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type {ReactNode} from 'react';
 import Translate, {translate} from '@docusaurus/Translate';
 
+import Head from '@docusaurus/Head';
 import Link from '@docusaurus/Link';
 import Layout from '@theme/Layout';
 import Heading from '@theme/Heading';
@@ -23,9 +24,16 @@ function ShowcaseHeader() {
   );
 }
 
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
 export default function Showcase(): ReactNode {
+  const imageUrl = useBaseUrl('/img/homepage-screenshot.png');
   return (
     <Layout title={TITLE} description={DESCRIPTION}>
+      <Head>
+        <meta property="og:image" content={imageUrl} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <main className="margin-vert--lg">
         <ShowcaseHeader />
         <ShowcaseFilters />
